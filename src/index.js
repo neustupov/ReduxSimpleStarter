aa822713e399003c95b7db97b2eab83e56b17374
neustupov <neustupov@yandex.ru>
2018-06-09 17:21:13 +0300

    Export Statements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBwYkIjzuwezNSZA_OBQ6N78VYJuQHQ0bI';
+
 const App = () => {
-    return <div>Hi!</div>
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
