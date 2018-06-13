3dfa24549acb2163e7686519e921266f06816f61
neustupov <neustupov@yandex.ru>
2018-06-13 23:44:46 +0300

    Searching for Videos(31)

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'Redux'}, (videos) => {
+        this.videoSearch('React');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos,
                 selectedVideo: videos[0]
@@ -28,7 +32,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
